bdaf0907b52b0f6aa3f3e0ccf36b9c48dadb99e6
Alpaslan <232arslan104@gmail.com>
2020-04-25 23:37:14 +0300

    Read Pin Setup

--- raspauto/__init__.py
+++ raspauto/__init__.py
@@ -20,47 +20,49 @@ class set:
         firebase = pyrebase.initialize_app(config)
         self.database = firebase.database()
         print("Baglantı Kuruldu")        
-    def auto(self,waittime,run):
-        while True:
-            self.getpins()
-            if run == False:
-                break
-            time.sleep(waittime)
-    def listen(self,str):
-        value = self.database.child(str).get().val()
+    def readdata(self,str):
+        value = self.database.child(self.name).child(str).get().val()
         return value
-    def setdata(self,pin,value):
-        self.database.child(self.name + "Connection").set(True)
+    def setdata(self,svar,svalue):
+        self.database.child(self.name).child(svar).set(svalue)
     def info(self):
         return float(open("/sys/class/thermal/thermal_zone0/temp", "r").readline())
     def getpins(self):
         try:    
             if os.name == "posix":
-                self.database.child(self.name + "_Connection").set(True)
-                self.database.child(self.name + "_Temperature").set(self.info())
-                value = self.database.child("pinsettings").get().val()
+                self.database.child(self.name).child(self.name + "_Connection").set(True)
+                self.database.child(self.name).child(self.name + "_Temperature").set(self.info())
+                value = self.database.child(self.name).child("pinsettings").get().val()
                 pinlist = value.split("#")
                 for itm in pinlist:
                     item = itm.split("$")
                     RPi.GPIO.setup(int(item[0]), RPi.GPIO.OUT)
                     if item[1] == "h":
                         RPi.GPIO.output(int(item[0]),RPi.GPIO.HIGH)
-                        print("high")
                     elif item[1] == "l":
                         RPi.GPIO.output(int(item[0]),RPi.GPIO.LOW)
-                        print("low")
-                    
             else:
                 print("Device is not a Raspberry.")
         except:
-            time.sleep(3)
+            time.sleep(2)
     def setpin(self,pin,value):
-        print(pin + type(pin))
-        print(value + type(value))
-        RPi.GPIO.setup(pin, RPi.GPIO.OUT)
-        if value == "h":
-            RPi.GPIO.output(pin,RPi.GPIO.HIGH)
-            print("high")
-        elif value == "l":
-            RPi.GPIO.output(pin,RPi.GPIO.LOW)
-            print("low")
+        try:
+            RPi.GPIO.setup(pin, RPi.GPIO.OUT)
+            if value == "h":
+                RPi.GPIO.output(pin,RPi.GPIO.HIGH)
+            elif value == "l":
+                RPi.GPIO.output(pin,RPi.GPIO.LOW)
+        except:
+            self.database.child(self.name).child("error_log_setpin").push("Error")    
+    def readpin(self,pin):
+        try: 
+            RPi.GPIO.setup(pin, RPi.GPIO.IN)
+            return RPi.GPIO.input(pin)
+        except:
+            self.database.child(self.name).child("error_log_readpin").push("Error")
+    def auto(self,waittime,run):
+        while True:
+            self.getpins()
+            if run == False:
+                break
+            time.sleep(waittime)
