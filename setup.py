cef09226b936c3e6b74d33bc41f567e883734368
Alpaslan Tetik <31161213+aattk@users.noreply.github.com>
2021-09-25 21:39:19 +0300

    Update setup.py
    - Removed OpenCV Library
    - Removed Numpy Library

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='raspauto',
-    version='0.2.2.3',
+    version='0.2.2.4',
     description='Raspberry Automation Library and Voice Recognition',
     long_description_content_type="text/markdown",
     long_description=README,
@@ -21,7 +21,7 @@ setup_args = dict(
 )
 
 install_requires = [
-    'python-telegram-bot','opencv-python','numpy'
+    'python-telegram-bot'
 ]
 
 if __name__ == '__main__':
