f9a1d9eec3c5b4261a5c7cfdfb42218893157557
aattk <232arslan104@gmail.com>
2021-09-25 22:04:38 +0300

    Update Version
    - System state update.
    - OpenCV & Numpy Removed.

--- raspauto/__init__.py
+++ raspauto/__init__.py
@@ -10,15 +10,17 @@ try:
     import RPi.GPIO as GPIO
     import picamera
 except Exception as e:
-    print("GPIO kütüphanesi bulunamadı")
+    print("GPIO Library not found.")
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 
 
-HOGCV = cv2.HOGDescriptor()
-HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
+# try:
+#     HOGCV = cv2.HOGDescriptor()
+#     HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# except Exception as e:
+#     print("OpenCV Library not found.")
 
 class set:
     def __init__(self, telegram_id, password):
@@ -212,36 +214,37 @@ class set:
                 self.users = self.users + [pin_temp]
                 pin_temp = []
                 i_counter = 0
-    def detect(self,frame):
-        bounding_box_cordinates, weights =  HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
-        
-        person = 1
-        for x,y,w,h in bounding_box_cordinates:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-            cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
-            person += 1
-        self.alarmpeople.message.reply_text('Please choose:')
-        cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
-        cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
-        cv2.imshow('output', frame)
-        return frame
 
-    def peopleTracer(self):
-        video = cv2.VideoCapture(0)
-        while self.alarm:
-            try:
-                print('Detecting people...')
-                check, frame = video.read()
-                frame = detect(frame)
-                cv2.imshow("Frame",frame)
-                key = cv2.waitKey(1)
-                if key == ord('q'):
-                    break
+    # def detect(self,frame):
+    #     bounding_box_cordinates, weights =  HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.03)
+        
+    #     person = 1
+    #     for x,y,w,h in bounding_box_cordinates:
+    #         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    #         cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+    #         person += 1
+    #     self.alarmpeople.message.reply_text('Please choose:')
+    #     cv2.putText(frame, 'Status : Detecting ', (40,40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    #     cv2.putText(frame, f'Total Persons : {person-1}', (40,70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,0,0), 2)
+    #     cv2.imshow('output', frame)
+    #     return frame
+
+    # def peopleTracer(self):
+    #     video = cv2.VideoCapture(0)
+    #     while self.alarm:
+    #         try:
+    #             print('Detecting people...')
+    #             check, frame = video.read()
+    #             frame = detect(frame)
+    #             cv2.imshow("Frame",frame)
+    #             key = cv2.waitKey(1)
+    #             if key == ord('q'):
+    #                 break
             
-            except Exception as e:
-                pass
-        video.release()
-        cv2.destroyAllWindows()
+    #         except Exception as e:
+    #             pass
+    #     video.release()
+    #     cv2.destroyAllWindows()
 
     def start(self,update, context):
         if self.login(update, context):
@@ -391,7 +394,3 @@ class set:
 
     def alarmstop(self,update,context):
         self.alarm = False
-
-
-
-    
